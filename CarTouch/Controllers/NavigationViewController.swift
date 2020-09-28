347e1b96abc75d7ee19b9dee6e8edfe2ca893029
Ruslan Lukmanov <ruslanlukmanov@MacBook-Pro-user.local>
2020-09-28 05:14:50 +0300

    28.09

---
+++ CarTouch/Controllers/NavigationViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationViewController.swift
+//  CarTouch
+//
+//  Created by Ruslan Lukmanov on 28.09.2020.
+//  Copyright © 2020 GangBank. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }    
+}
